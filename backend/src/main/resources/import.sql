00a0c60436c3299e46779ab61d100614f0fded5b
Aline Paz <aline.soares.2@hotmail.com>
2021-02-17 18:40:28 -0300

    Database seeding]

---
+++ backend/src/main/resources/import.sql
@@ -0,0 +1,4 @@
+INSERT INTO tb_client (name) VALUES ('ALINE');
+INSERT INTO tb_client (name) VALUES ('ELIS');
+INSERT INTO tb_client (name) VALUES ('REGINA');
+INSERT INTO tb_client (name) VALUES ('FRANSCISCO');
