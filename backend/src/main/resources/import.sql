c49e65f9d9c61a9b853ea0c48f11d81a44b25590
Aline Paz <aline.soares.2@hotmail.com>
2021-03-02 18:06:32 -0300

    import sql

--- backend/src/main/resources/import.sql
+++ backend/src/main/resources/import.sql
@@ -1,10 +1,11 @@
-INSERT INTO tb_client (name, cpf, income, birth_date, children) VALUES ('ALINE', '45327215806', 6500.0, TIMESTAMP WITH TIME ZONE '2020-07-14T10:00:00Z', 0);
-INSERT INTO tb_client (name, cpf, income, birth_date, children) VALUES ('ROBERTA', '45327215806', 6500.0, TIMESTAMP WITH TIME ZONE '2020-07-14T10:00:00Z', 4);
-INSERT INTO tb_client (name, cpf, income, birth_date, children) VALUES ('ALICE', '45327215806', 6500.0, TIMESTAMP WITH TIME ZONE '2020-07-14T10:00:00Z', 3);
-INSERT INTO tb_client (name, cpf, income, birth_date, children) VALUES ('PAULA', '45327215806', 6500.0, TIMESTAMP WITH TIME ZONE '2020-07-14T10:00:00Z', 2);
-INSERT INTO tb_client (name, cpf, income, birth_date, children) VALUES ('ALFREDO', '45327215806', 6500.0, TIMESTAMP WITH TIME ZONE '2020-07-14T10:00:00Z', 1);
-INSERT INTO tb_client (name, cpf, income, birth_date, children) VALUES ('JORGE', '45327215806', 6500.0, TIMESTAMP WITH TIME ZONE '2020-07-14T10:00:00Z', 2);
-INSERT INTO tb_client (name, cpf, income, birth_date, children) VALUES ('ELIS', '45327215806', 6500.0, TIMESTAMP WITH TIME ZONE '2020-07-14T10:00:00Z', 4);
-INSERT INTO tb_client (name, cpf, income, birth_date, children) VALUES ('REGINA', '45327215806', 6500.0, TIMESTAMP WITH TIME ZONE '2020-07-14T10:00:00Z', 7);
-INSERT INTO tb_client (name, cpf, income, birth_date, children) VALUES ('MARIA FLOR', '45327215806', 6500.0, TIMESTAMP WITH TIME ZONE '2020-07-14T10:00:00Z', 5);
-INSERT INTO tb_client (name, cpf, income, birth_date, children) VALUES ('LUCIA', '45327215806', 6500.0, TIMESTAMP WITH TIME ZONE '2020-07-14T10:00:00Z', 2);
+
+INSERT INTO tb_client (name, cpf, income, birth_date, children) VALUES ('REGINA', '45327715809', 9900.0, TIMESTAMP WITH TIME ZONE '2020-07-14T10:00:00Z', 7);
+INSERT INTO tb_client (name, cpf, income, birth_date, children) VALUES ('MARIA FLOR', '45327295806', 9990.0, TIMESTAMP WITH TIME ZONE '2020-07-14T10:00:00Z', 5);
+INSERT INTO tb_client (name, cpf, income, birth_date, children) VALUES ('LUCIA', '45325215866', 6900.0, TIMESTAMP WITH TIME ZONE '2020-07-14T10:00:00Z', 2);
+INSERT INTO tb_client (name, cpf, income, birth_date, children) VALUES ('FELIPE', '45327115801', 6500.0, TIMESTAMP WITH TIME ZONE '2020-07-14T10:00:00Z', 3);
+INSERT INTO tb_client (name, cpf, income, birth_date, children) VALUES ('ALINE', '45327215123', 9870.0, TIMESTAMP WITH TIME ZONE '2020-07-14T10:00:00Z', 4);
+INSERT INTO tb_client (name, cpf, income, birth_date, children) VALUES ('ELIS', '12327215806', 7600.0, TIMESTAMP WITH TIME ZONE '2020-07-14T10:00:00Z', 0);
+INSERT INTO tb_client (name, cpf, income, birth_date, children) VALUES ('PAULA', '45380715806', 9500.0, TIMESTAMP WITH TIME ZONE '2020-07-14T10:00:00Z', 2);
+INSERT INTO tb_client (name, cpf, income, birth_date, children) VALUES ('ANA FLOR', '45325675806', 8500.0, TIMESTAMP WITH TIME ZONE '2020-07-14T10:00:00Z', 1);
+INSERT INTO tb_client (name, cpf, income, birth_date, children) VALUES ('JULIA', '45327215606', 9800.0, TIMESTAMP WITH TIME ZONE '2020-07-14T10:00:00Z', 2);
+INSERT INTO tb_client (name, cpf, income, birth_date, children) VALUES ('LUCIANA', '45816215806', 7800.0, TIMESTAMP WITH TIME ZONE '2020-07-14T10:00:00Z', 5);
